c52ce8ab1027f462feed59549059ee737931e1c8
iamtienng <iamtienng@gmail.com>
2025-01-09 16:07:55 +0100

    feat: update config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,33 +1,5 @@
 return {
-  {
-    -- Brief Aside: **What is LSP?**
-    --
-    -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-    -- and language tooling communicate in a standardized fashion.
-    --
-    -- In general, you have a "server" which is some tool built to understand a particular
-    -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc). These Language Servers
-    -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-    -- processes that communicate with some "client" - in this case, Neovim!
-    --
-    -- LSP provides Neovim with features like:
-    --  - Go to definition
-    --  - Find references
-    --  - Autocompletion
-    --  - Symbol Search
-    --  - and more!
-    --
-    -- Thus, Language Servers are external tools that must be installed separately from
-    -- Neovim. This is where `mason` and related plugins come into play.
-
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-
-    -- NOTE: nvim-lspconfig already init with LazyVim by default, so this is the extra configs
+  { -- NOTE: nvim-lspconfig already init with LazyVim by default, so this is the extra configs
     -- https://www.lazyvim.org/plugins/lsp#nvim-lspconfig
     "neovim/nvim-lspconfig",
     dependencies = {
