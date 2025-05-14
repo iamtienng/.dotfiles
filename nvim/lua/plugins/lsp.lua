0d23151b53d49c29a349ac17ff62e21184fe6dcc
Tien Nguyen <iamtienng@gmail.com>
2025-05-14 23:20:13 +0200

    feat: update config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -27,11 +27,13 @@ return {
   },
   {
     "williamboman/mason.nvim",
+    version = "1.11.0",
     opts = {
       ui = {
         border = "rounded",
       },
       ensure_installed = { "tflint" },
     },
+    { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
   },
 }
