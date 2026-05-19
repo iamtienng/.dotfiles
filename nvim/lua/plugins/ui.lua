2ee0c8a33f6f543f68de8e6813f04aafb32e579e
Tien Nguyen <iamtienng@gmail.com>
2026-05-19 15:45:48 +0200

    fix(nvim): ui config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -101,16 +101,35 @@ return {
 
         local ok_bufferline, bufferline = pcall(require, "bufferline")
         if ok_bufferline then
-          vim.opt.hidden = true
           bufferline.setup({
             options = {
+              -- stylua: ignore
+              close_command = function(n)Snacks.bufdelete(n) end,
+              -- stylua: ignore
+              right_mouse_command = function(n) Snacks.bufdelete(n) end,
+              diagnostics = "nvim_lsp",
+              always_show_bufferline = false,
+              diagnostics_indicator = function(_, _, diag)
+                local icons = LazyVim.config.icons.diagnostics
+                local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+                  .. (diag.warning and icons.Warn .. diag.warning or "")
+                return vim.trim(ret)
+              end,
               offsets = {
                 {
                   filetype = "neo-tree",
-                  text = "Explorer",
-                  separator = true,
+                  text = "Neo-tree",
+                  highlight = "Directory",
+                  text_align = "left",
+                },
+                {
+                  filetype = "snacks_layout_box",
                 },
               },
+              ---@param opts bufferline.IconFetcherOpts
+              get_element_icon = function(opts)
+                return LazyVim.config.icons.ft[opts.filetype]
+              end,
             },
           })
         end
